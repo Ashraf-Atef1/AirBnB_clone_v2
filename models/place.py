11f4997cc7681718d86d9ba6cd292a2e88ad6e25
Ashraf Atef <ashraf.programer@gmail.com>
2024-04-16 12:53:36 +0200

    task

--- models/place.py
+++ models/place.py
@@ -2,16 +2,10 @@
 """Defines the Place class."""
 import models
 from os import getenv
-from models.base_model import Base
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from models.amenity import Amenity
 from models.review import Review
-from sqlalchemy import Column
-from sqlalchemy import Float
-from sqlalchemy import ForeignKey
-from sqlalchemy import Integer
-from sqlalchemy import String
-from sqlalchemy import Table
+from sqlalchemy import Column, Float, ForeignKey,Integer ,String, Table
 from sqlalchemy.orm import relationship
 
 
