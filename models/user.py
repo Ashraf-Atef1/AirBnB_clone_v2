6858e48bc5890212c17a38a889c224ab23e323d3
Ashraf Atef <ashraf.programer@gmail.com>
2024-04-18 11:27:18 +0200

    task

--- models/user.py
+++ models/user.py
@@ -1,33 +1,37 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""Defines the User class."""
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
-if getenv("HBNB_TYPE_STORAGE") == "db":
+class User(BaseModel, Base):
+    """Represents a user in a lodging system.
 
-    class User(BaseModel, Base):
-        """This class defines a user by various attributes"""
+    Inherits from BaseModel and links to the MySQL table 'users'.
+    It stores information about users, including their personal details,
+    places they own, and reviews they've written.
 
-        __tablename__ = "users"
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store users.
+        email (sqlalchemy String): The email address of the user.
+        password (sqlalchemy String): The password of the user.
+        first_name (sqlalchemy String): The first name of the user.
+        last_name (sqlalchemy String): The last name of the user.
+        places (sqlalchemy relationship):
+        Relationship with Place class to represent
+            the places owned by the user.
+        reviews (sqlalchemy relationship):
+        Relationship with Review class to represent
+            the reviews written by the user.
+    """
 
-        places = relationship(
-            "Place", back_populates="user", cascade="all, delete-orphan"
-        )
-        reviews = relationship(
-            "Review", back_populates="user", cascade="all, delete-orphan"
-        )
+    __tablename__ = "users"
 
-else:
-    class User(BaseModel):
-        """This class defines a user by various attributes"""
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
