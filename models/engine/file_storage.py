1cb5b291f0776d3764a58e769b00026d72dbee5b
Ashraf Atef <ashraf.programer@gmail.com>
2024-04-18 11:16:44 +0200

    task

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,82 +1,99 @@
 #!/usr/bin/python3
-"""Defines the FileStorage class."""
+"""This module defines a class to manage file storage for hbnb clone"""
 import json
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 
 
 class FileStorage:
-    """Represents a file-based storage engine.
-
-    Attributes:
-        __file_path (str): The path to the JSON file storing objects.
-        __objects (dict): A dictionary of objects keyed by class name and ID.
-    """
+    """This class manages storage of hbnb models in JSON format"""
 
     __file_path = "file.json"
     __objects = {}
 
+    @classmethod
     def all(self, cls=None):
-        """Retrieve all objects or objects of a specific class.
+        """Returns a dictionary of models currently in storage"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
 
-        Args:
-            cls (class, optional): The class to filter objects.
-
-        Returns:
-            dict: A dictionary of objects.
-        """
-        if cls is not None:
-            if isinstance(cls, str):
-                cls = eval(cls)
-            cls_dict = {}
-            for key, obj in self.__objects.items():
-                if isinstance(obj, cls):
-                    cls_dict[key] = obj
-            return cls_dict
-        return self.__objects
+        classes = {
+            "BaseModel": BaseModel,
+            "User": User,
+            "Place": Place,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Review": Review,
+        }
+        required_objs = {}
+        if cls:
+            if cls not in classes.keys():
+                return required_objs
+            for key, value in FileStorage.all().items():
+                if key.split(".")[0] == cls:
+                    # print(key.split('.')[0])
+                    required_objs.append(value)
+                    # print(value)
+                # print (required_objs)
+            return required_objs
+        else:
+            return FileStorage.__objects
 
     def new(self, obj):
-        """Add a new object to the storage dictionary.
-
-        Args:
-            obj (BaseModel): The object to add.
-        """
-        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
+        """Adds new object to storage dictionary"""
+        self.all().update({obj.to_dict()["__class__"] + "." + obj.id: obj})
 
     def save(self):
-        """Serialize the storage dictionary to a JSON file."""
-        serialized_objs = {key: obj.to_dict()
-                           for key, obj in self.__objects.items()}
-        with open(self.__file_path, "w", encoding="utf-8") as file:
-            json.dump(serialized_objs, file)
+        """Saves storage dictionary to file"""
+        with open(FileStorage.__file_path, "w") as f:
+            temp = {}
+            temp.update(FileStorage.__objects)
+            for key, val in temp.items():
+                temp[key] = val.to_dict()
+            json.dump(temp, f, indent=4)
 
     def reload(self):
-        """Deserialize the JSON file to populate the storage dictionary."""
+        """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
+            "BaseModel": BaseModel,
+            "User": User,
+            "Place": Place,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Review": Review,
+        }
         try:
-            with open(self.__file_path, "r", encoding="utf-8") as file:
-                data = json.load(file)
-                for key, obj_data in data.items():
-                    class_name, obj_id = key.split('.')
-                    cls = eval(class_name)
-                    self.__objects[key] = cls(**obj_data)
+            temp = {}
+            with open(FileStorage.__file_path, "r") as f:
+                temp = json.load(f)
+                for key, val in temp.items():
+                    # object created appended to __objects
+                    # print (classes[val['__class__']])
+                    self.all()[key] = classes[val["__class__"]](**val)
         except FileNotFoundError:
             pass
 
+        # print("priting what's isnnide objetcts" + str(self.all()))
+
     def delete(self, obj=None):
-        """Delete an object from the storage dictionary.
-
-        Args:
-            obj (BaseModel, optional): The object to delete.
-        """
-        if obj is not None:
-            key = "{}.{}".format(type(obj).__name__, obj.id)
-            self.__objects.pop(key, None)
-
-    def close(self):
-        """Reload data from file."""
-        self.reload()
+        """delete obj from __objects if its inside - if
+        obj is equal to None, the method should not do anything"""
+        if obj == None:
+            return
+        for key in self.all().keys():
+            if key.split(".")[1] == obj.id:
+                del self.all()[key]
+                break
--- test.py
+++ test.py
@@ -1,19 +1,50 @@
 #!/usr/bin/python3
-
+""" Test delete feature
+"""
+from models.engine.file_storage import FileStorage
 from models.state import State
-from models.city import City
-from models import storage
 
+fs = FileStorage()
 
-new_state = State(name="California")
-storage.new(new_state)
+# All States
+all_states = fs.all(State)
+print("All States: {}".format(len(all_states.keys())))
+for state_key in all_states.keys():
+    print(all_states[state_key])
 
-# new_cities = []
-# new_city1 = City(name="San Francisco")
-# new_cities.append(new_city1)
-# new_city2 = City(name="Los Angeles")
-# new_cities.append(new_city2)
+# Create a new State
+new_state = State()
+new_state.name = "California"
+fs.new(new_state)
+fs.save()
+print("New State: {}".format(new_state))
 
-storage.save()
+# All States
+all_states = fs.all(State)
+print("All States: {}".format(len(all_states.keys())))
+for state_key in all_states.keys():
+    print(all_states[state_key])
 
+# Create another State
+another_state = State()
+another_state.name = "Nevada"
+fs.new(another_state)
+fs.save()
+print("Another State: {}".format(another_state))
+
+# All States
+all_states = fs.all(State)
+print("All States: {}".format(len(all_states.keys())))
+for state_key in all_states.keys():
+    print(all_states[state_key])        
+
+# Delete the new State
+fs.delete(new_state)
+
+# All States
+all_states = fs.all(State)
+print("All States: {}".format(len(all_states.keys())))
+for state_key in all_states.keys():
+    print(all_states[state_key])
+    
 
