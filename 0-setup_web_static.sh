e4d8129eacd34e0c267d8207f76f243271f45e4f
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-05-03 04:24:49 +0300

    Task

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #  Bash script that sets up the web servers for the deployment of web_static
 mkdir -p /data/web_static/releases/test/ /data/web_static/shared/ 
-echo "<html>\n<head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" > /data/web_static/releases/test/index.html
+echo "<html><head></head><body>Holberton School</body></html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown ubuntu /data/
 chgrp ubuntu /data/
