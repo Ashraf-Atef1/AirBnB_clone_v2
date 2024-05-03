2d5832e8286e47b552affe4fb3a2475de99527fb
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-05-03 04:29:49 +0300

    Task

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 #  Bash script that sets up the web servers for the deployment of web_static
+sudo su
 mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
-sudo chmod 777 /data/web_static/releases/test
+chmod 777 /data/web_static/releases/test
 echo "<html><head></head><body>Holberton School</body></html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown ubuntu /data/
 chgrp ubuntu /data/
 sed -i '26i\	location /hbnb_static {\n\t alias /data/web_static/current/;\n} ' /etc/nginx/sites-available/default
+exit
