d82049b3a84a37a06df7a6ab0a9436b3593ae674
Ashraf Atef <ashraf.programer@gmail.com>
2024-05-04 02:19:47 +0300

    task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,12 +17,14 @@ def do_deploy(archive_path=None):
             path = "/data/web_static/releases/"
             put(archive_path, '/tmp/')
             run('mkdir -p {}{}/'.format(path, file_no_ext))
-            run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, file_no_ext))
+            run('tar -xzf /tmp/{} -C {}{}/'.format(file_name,
+                                                   path, file_no_ext))
             run('rm /tmp/{}'.format(file_name))
             run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_no_ext))
             run('rm -rf {}{}/web_static'.format(path, file_no_ext))
             run('rm -rf /data/web_static/current')
-            run('ln -s {}{}/ /data/web_static/current'.format(path, file_no_ext))
+            run('ln -s {}{}/ /data/web_static/current'.format(
+                path, file_no_ext))
         else:
             return False
     except Exception:
