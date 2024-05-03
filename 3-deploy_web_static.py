717f2ed70f4e976be276eaec5bdb3a5a335aa1ce
Ashraf Atef <ashraf.programer@gmail.com>
2024-05-04 02:33:45 +0300

    task

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers """
+
+import time
+from fabric.api import run, put, env, local
+from os.path import exists, isdir
+env.hosts = ['100.25.12.124', '54.236.45.187']
+
+
+def do_pack():
+    """ A function that generates a .tgz archive """
+    try:
+        file_name = "versions/web_static_{}.tgz".format(
+            time.strftime("%Y%m%d%H%M%S"))
+        if isdir("versions") is False:
+            local("mkdir versions")
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except Exception:
+        return None
+
+
+def do_deploy(archive_path):
+    """ A function that distributes an archive to your web servers """
+    try:
+        if exists(archive_path):
+            file_name = archive_path.split("/")[-1]
+            file_no_ext = file_name.split(".")[0]
+            path = "/data/web_static/releases/"
+            put(archive_path, '/tmp/')
+            run('mkdir -p {}{}/'.format(path, file_no_ext))
+            run('tar -xzf /tmp/{} -C {}{}/'.format(file_name,
+                                                   path, file_no_ext))
+            run('rm /tmp/{}'.format(file_name))
+            run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_no_ext))
+            run('rm -rf {}{}/web_static'.format(path, file_no_ext))
+            run('rm -rf /data/web_static/current')
+            run('ln -s {}{}/ /data/web_static/current'.format(
+                path, file_no_ext))
+            return True
+        else:
+            return False
+    except Exception:
+        return False
+
+
+def deploy():
+    """ A function that distributes an archive to your web servers """
+    return do_deploy(do_pack())
