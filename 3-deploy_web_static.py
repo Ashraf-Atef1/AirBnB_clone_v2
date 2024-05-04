3f11e0968faff153154eb3845233664c8f15b65e
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-05-05 01:25:10 +0300

    task3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,7 +3,7 @@
 that distributes an archive to your web servers """
 
 import time
-from fabric.api import run, put, env, local
+from fabric.api import sudo, put, env, local
 from os.path import exists, isdir
 
 
@@ -28,14 +28,14 @@ def do_deploy(archive_path):
             file_no_ext = file_name.split(".")[0]
             path = "/data/web_static/releases/"
             put(archive_path, '/tmp/')
-            run('mkdir -p {}{}/'.format(path, file_no_ext))
-            run('tar -xzf /tmp/{} -C {}{}/'.format(file_name,
+            sudo('mkdir -p {}{}/'.format(path, file_no_ext))
+            sudo('tar -xzf /tmp/{} -C {}{}/'.format(file_name,
                                                    path, file_no_ext))
-            run('rm /tmp/{}'.format(file_name))
-            run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_no_ext))
-            run('rm -rf {}{}/web_static'.format(path, file_no_ext))
-            run('rm -rf /data/web_static/current')
-            run('ln -s {}{}/ /data/web_static/current'.format(
+            sudo('rm /tmp/{}'.format(file_name))
+            sudo('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_no_ext))
+            sudo('rm -rf {}{}/web_static'.format(path, file_no_ext))
+            sudo('rm -rf /data/web_static/current')
+            sudo('ln -s {}{}/ /data/web_static/current'.format(
                 path, file_no_ext))
             return True
         else:
