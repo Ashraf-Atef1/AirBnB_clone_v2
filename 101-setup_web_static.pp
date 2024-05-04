21a93fb64a5159aee725cbd3258ee85b12cf0929
Ashraf Atef <ashraf.programer@gmail.com>
2024-05-04 03:42:28 +0300

    task

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,54 +1,87 @@
-# Puppet manifest to set up web servers for the deployment of web_static
+# Puppet for setup
 
-# Update package repositories
-exec { 'apt-update':
-  command => '/usr/bin/apt-get update',
-}
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://linktr.ee/firdaus_h_salim/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
 
-# Install nginx package
 package { 'nginx':
-  ensure => 'installed',
+  ensure   => 'present',
+  provider => 'apt'
 }
 
-# Create directories
-file { '/data/web_static/releases/test/':
-  ensure => 'directory',
-  mode   => '0777',
+-> file { '/data':
+  ensure  => 'directory'
 }
 
-file { '/data/web_static/shared/':
-  ensure => 'directory',
+-> file { '/data/web_static':
+  ensure => 'directory'
 }
 
-# Create index.html
-file { '/data/web_static/releases/test/index.html':
-  ensure  => 'file',
-  content => '<html><head></head><body>Holberton School</body></html>',
+-> file { '/data/web_static/releases':
+  ensure => 'directory'
 }
 
-# Create symlink
-file { '/data/web_static/current':
+-> file { '/data/web_static/releases/test':
+  ensure => 'directory'
+}
+
+-> file { '/data/web_static/shared':
+  ensure => 'directory'
+}
+
+-> file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "this webpage is found in data/web_static/releases/test/index.htm \n"
+}
+
+-> file { '/data/web_static/current':
   ensure => 'link',
-  target => '/data/web_static/releases/test/',
+  target => '/data/web_static/releases/test'
 }
 
-# Set ownership
-exec { 'set-ownership':
-  command => 'chown -R ubuntu:ubuntu /data/',
+-> exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
-# Add nginx configuration
-file_line { 'add-hbnb-static':
-  line    => '	location /hbnb_static { alias /data/web_static/current/; }',
-  path    => '/etc/nginx/sites-available/default',
-  match   => '^.*location \/hbnb_static.*$',
-  ensure  => present,
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+file { '/var/www':
+  ensure => 'directory'
 }
 
-# Start nginx service
-service { 'nginx':
-  ensure => 'running',
-  enable => 'true',
+-> file { '/var/www/html':
+  ensure => 'directory'
+}
+
+-> file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "This is my first upload  in /var/www/index.html***\n"
+}
+
+-> file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page - Error page\n"
+}
+
+-> file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
+}
+
+-> exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
