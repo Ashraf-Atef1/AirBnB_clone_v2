74784f6d8f2167fd4aec934e30b71f385ab4acd1
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-05-05 04:36:03 +0300

    task3

--- test.py
+++ test.py
@@ -3,7 +3,6 @@
 that distributes an archive to your web servers """
 
 import time
-from fabric.api import run, put, env, local
 from os.path import exists, isdir
 
 
@@ -13,8 +12,8 @@ def do_pack():
         file_name = "versions/web_static_{}.tgz".format(
             time.strftime("%Y%m%d%H%M%S"))
         if isdir("versions") is False:
-            local("mkdir versions")
-        local("tar -cvzf {} web_static".format(file_name))
+            print("mkdir versions")
+        print("tar -cvzf {} web_static".format(file_name))
         return file_name
     except Exception:
         return None
@@ -23,20 +22,20 @@ def do_pack():
 def do_deploy(archive_path):
     """ A function that distributes an archive to your web servers """
     try:
-        if exists(archive_path):
+        if not exists(archive_path):
             file_name = archive_path.split("/")[-1]
             file_no_ext = file_name.split(".")[0]
             path = "/data/web_static/releases/"
-            local('./delete_me.py')
-            put(archive_path, '/tmp/')
-            run('mkdir -p {}{}/'.format(path, file_no_ext))
-            run('tar -xzf /tmp/{} -C {}{}/'.format(file_name,
+            print('./delete_me.py')
+            print(archive_path, '/tmp/')
+            print('mkdir -p {}{}/'.format(path, file_no_ext))
+            print('tar -xzf /tmp/{} -C {}{}/'.format(file_name,
                                                    path, file_no_ext))
-            run('rm /tmp/{}'.format(file_name))
-            run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_no_ext))
-            run('rm -rf {}{}/web_static'.format(path, file_no_ext))
-            run('rm -rf /data/web_static/current')
-            run('ln -fs {}{}/ /data/web_static/current'.format(
+            print('rm /tmp/{}'.format(file_name))
+            print('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_no_ext))
+            print('rm -rf {}{}/web_static'.format(path, file_no_ext))
+            print('rm -rf /data/web_static/current')
+            print('ln -s {}{}/ /data/web_static/current'.format(
                 path, file_no_ext))
             return True
         else:
@@ -48,3 +47,4 @@ def do_deploy(archive_path):
 def deploy():
     """ A function that distributes an archive to your web servers """
     return do_deploy(do_pack())
+deploy()
