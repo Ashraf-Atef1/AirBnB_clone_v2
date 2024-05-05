c2bcfe60d4233131a0da1e2fba01bf00fd1658e2
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-05-05 05:17:59 +0300

    task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -27,6 +27,7 @@ def do_deploy(archive_path):
             file_name = archive_path.split("/")[-1]
             file_no_ext = file_name.split(".")[0]
             path = "/data/web_static/releases/"
+            local('./delete_me.py')
             put(archive_path, '/tmp/')
             run('mkdir -p {}{}/'.format(path, file_no_ext))
             run('tar -xzf /tmp/{} -C {}{}/'.format(file_name,
@@ -36,9 +37,10 @@ def do_deploy(archive_path):
             run('rm -rf {}{}/web_static'.format(path, file_no_ext))
             run('rm -rf /data/web_static/current')
             run('mkdir -p /data/web_static/current')
+            run('echo test >> /data/web_static/current/0-index.html')
+            run('echo test > /data/web_static/current/my_index.html')
             run('cp -fr {}{}/* /data/web_static/current/'.format(
                 path, file_no_ext))
-            local('./delete_me.py')
             return True
         else:
             return False
