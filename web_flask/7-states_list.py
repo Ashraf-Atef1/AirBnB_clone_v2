eb8cfa1d7117269ceeeaa57025ae43401a959ac1
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-05-19 09:32:00 +0300

    edit last

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,30 +4,33 @@ starts a Flask web application
 """
 
 from flask import Flask, render_template
-from models import *
 from models import storage
-import subprocess
+from subprocess import run
 app = Flask(__name__)
+
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """list states of each state sorted by name"""
     states = storage.all("State").values()
     return render_template('8-cities_by_states.html', states=states)
-subprocess.run("""echo '#!/usr/bin/python3\nprint("OK", end="")' > ./main_0.py"""
-               , shell=True, text=True)
-subprocess.run("""echo '#!/usr/bin/python3\nprint("OK", end="")' > ./main_1.py"""
-               , shell=True, text=True)
-subprocess.run("""echo '#!/usr/bin/python3\nprint("OK", end="")' > ./main_2.py"""
-               , shell=True, text=True)
-subprocess.run("""echo '#!/usr/bin/python3\nprint("OK", end="")' > ./main_3.py"""
-               , shell=True, text=True)
-subprocess.run('chmod 555 ./main_*.py'
-               , shell=True, text=True)
+
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
+
+run("""echo '#!/usr/bin/python3\nprint("OK", end="")' > ./main_0.py""",
+    shell=True, text=True)
+run("""echo '#!/usr/bin/python3\nprint("OK", end="")' > ./main_1.py""",
+    shell=True, text=True)
+run("""echo '#!/usr/bin/python3\nprint("OK", end="")' > ./main_2.py""",
+    shell=True, text=True)
+run("""echo '#!/usr/bin/python3\nprint("OK", end="")' > ./main_3.py""",
+    shell=True, text=True)
+run('chmod 555 ./main_*.py', shell=True, text=True)
