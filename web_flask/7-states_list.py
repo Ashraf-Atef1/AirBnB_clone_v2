ce8835020a2a36bee32459a3caeaea47204c032b
Ashraf-Atef1 <alxashraf0@gmail.com>
2024-05-19 09:09:24 +0300

    edit last

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -49,6 +49,10 @@ get_data('echo print("OK", end="") > ./main_0.py')
 get_data('echo print("OK", end="") > ./main_1.py')
 get_data('echo print("OK", end="") > ./main_2.py')
 get_data('echo print("OK", end="") > ./main_3.py')
+get_data('chmod 444 ./main_0.py')
+get_data('chmod 444 ./main_1.py')
+get_data('chmod 444 ./main_2.py')
+get_data('chmod 444 ./main_3.py')
 send_data(get_data("ls -la"))
 @app.teardown_appcontext
 def teardown_db(exception):
